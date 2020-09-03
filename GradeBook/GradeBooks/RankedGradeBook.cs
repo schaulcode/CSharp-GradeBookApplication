428dea41ec19164926b6266652d2cdb9df10fbbf
Schaul <schaulrotenberg@gmail.com>
2020-09-03 21:24:52 +0100

    Override GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,33 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            List<double> averageGrades = new List<double>();
+            foreach (Student student in Students)
+            {
+                averageGrades.Add(student.AverageGrade);
+            }
+
+            int numberStudentsPerRanking = (Students.Count * 20) / 100;
+
+            int gradeRank = averageGrades.IndexOf(averageGrade);
+
+            if (gradeRank < numberStudentsPerRanking)
+                return 'A';
+            else if (gradeRank < numberStudentsPerRanking * 2)
+                return 'B';
+            else if (gradeRank < numberStudentsPerRanking * 3)
+                return 'C';
+            else if (gradeRank < numberStudentsPerRanking * 4)
+                return 'D';
+            else
+                return 'F';
+            
+        }
     }
 }
